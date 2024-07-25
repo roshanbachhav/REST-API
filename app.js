082e17d8e1d9b55c29546489c04129807dc130b0
roshanbachhav <bachhavroshan600@gmail.com>
2024-07-25 15:59:58 +0530

    Resolved merge conflicts

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-// import express from "express"
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 1000
+const PORT = process.env.PORT || 8080;
 
-const data_routes = require("./Routes/Data")
+const data_routes = require("./Routes/Data");
+const dbConnect = require("./Database/conn");
 
-const dbConnect = require("./Database/conn")
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+  })
+);
+
 
 app.get("/", (request, response) => {
-    console.log("Received a request at /");
-    response.send("Welcome to root path of api");
+  console.log("Received a request at /");
+  response.send("Welcome to root path of API");
 });
 
-app.use("/api/data" , data_routes);
+
+app.use("/api/data", data_routes);
 
 const start = async () => {
-    try{
-        await dbConnect();
-        
-        app.listen(PORT, () =>{
-            console.log(`${PORT} is live`);
-        })
-    }catch(e){
-        console.log(e);
-    }
+  try {
+    await dbConnect();
+    app.listen(PORT, () => {
+      console.log(`${PORT} is live`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 start();
